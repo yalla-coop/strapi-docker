cbdffea6f07834c6e16b76142ec19c79875317a3
Ramy Shurafa <ramyshurafa@hotmail.com>
2023-12-17 22:40:18 +0300

    using compose file 3

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 const path = require("path");
 
 module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "postgres");
+  const client = "postgres";
 
   const connections = {
     postgres: {
       connection: {
         connectionString: env("DATABASE_URL"),
         host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
+        port: 5432,
         database: env("DATABASE_NAME", "strapi"),
         user: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi"),
